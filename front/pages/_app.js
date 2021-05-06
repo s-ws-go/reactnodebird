9767f8440a1c3ae92a19fd3d835c76079ba1b908
s-ws-go <aplord@naver.com>
2021-05-06 21:34:16 +0900

    test

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import Head from 'next/head';
-import PropTypes from 'prop-types';
-import 'antd/dist/antd.css';
+import React from "react";
+import Head from "next/head";
+import PropTypes from "prop-types";
+import "antd/dist/antd.css";
 
 const NodeBird = ({ Component }) => {
   return (
     <>
       <Head>
         <title>NodeBird</title>
+        {/* 깃허브 커밋 등록 되는지 테스트 */}
       </Head>
       <Component />
     </>
